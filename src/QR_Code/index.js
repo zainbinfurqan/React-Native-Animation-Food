ff2c3c2e0cc4133b4d57665207d0cd44509cf7c1
zainbinfurqan <zain.ahmed199524@gmail.com>
2020-10-11 01:01:50 +0500

    udpate

---
+++ src/QR_Code/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native'
+
+function QRCode(props) {
+    return (
+        <View style={{ borderWidth: 2, flex: 1 }}>
+
+        </View>
+    );
+}
+
+export default QRCode;
